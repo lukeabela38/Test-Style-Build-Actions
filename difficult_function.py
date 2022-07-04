a70d7fe91472c325a3fe98bfd0bd5588e5cb9c55
luke <luke@smartcow.ai>
2022-07-04 14:18:29 +0200

    adding new files in staging area

---
+++ difficult_function.py
@@ -0,0 +1,15 @@
+def difficult_function(a, b):    
+    if a > 0:        
+        if b > 0:            
+            if isinstance(a,int):                
+                if isinstance(b,int):                    
+                    c = a + b                    
+                    return c               
+                else:                    
+                    pass            
+            else:                
+                pass        
+        else:            
+            pass    
+    else:               
+        pass
---
+++ easy_function.py
@@ -0,0 +1,6 @@
+def easy_function(a, b):    
+    if a > 0 and b > 0:       
+        c = a + b    
+    else:
+        c = 0
+    return c
